616a476acf27a17f90fbfc9c95cc9ab2aadd6659
Ajarek <ajarek@poczta.onet.pl>
2024-11-25 00:38:42 +0100

    added process.cwd()

--- lib/fetch.tsx
+++ lib/fetch.tsx
@@ -1,8 +1,12 @@
 "use server"
 import fs from "fs/promises"
 import type { Note } from "@/types/typeNote"
+import path from "path"
 
 export const fetchNotes = async () => {
-  const notesData = await fs.readFile("data/notes.json", "utf8")
+  const notesData = await fs.readFile(
+    path.resolve(process.cwd(), "data/notes.json"),
+    "utf8"
+  )
   return JSON.parse(notesData) as Note[]
 }
